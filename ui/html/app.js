9be1a72d732aa89812399909aabd941a9417dd2d
Shavasana11 <82469531+Shavasana11@users.noreply.github.com>
2023-03-13 01:24:47 +0300

    13/3/23
    ну все что смогла

--- ui/html/app.js
+++ ui/html/app.js
@@ -1,3 +1,12 @@
+const maps = document.querySelectorAll(".map");
+
+maps.forEach(map => {
+    map.addEventListener('click', () => {
+        map.classList.toggle('active');
+    });
+});
+
+
 const faqs = document.querySelectorAll(".faq");
 
 faqs.forEach(faq => {
